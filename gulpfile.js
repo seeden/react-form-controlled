50ec1f150f3dd14d94470f2a58960e38183d2450
Zlatko Fedor <zfedor@gmail.com>
2015-11-01 23:01:39 +0100

    Release 0.9.24

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,21 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
-var babelCompiler = require('babel/register');
 var babel = require('gulp-babel');
 
 gulp.task('test', function () {
-  return gulp.src('./tests/**/*.{js,jsx}')
-    .pipe(mocha({
-      compilers: {
-        js: babelCompiler
-      },
-      timeout: 5000
-    }));
+  return gulp.src('./tests/**/*.js')
+  .pipe(babel())
+  .pipe(mocha({
+    timeout: 20000
+  }));
 });
 
 gulp.task('build', function (callback) {
-  return gulp.src('./src/**/*.{js,jsx}')
+  return gulp.src('./src/**/*.js')
     .pipe(babel({
       stage: 0
     }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest("./dist"));
 });
 
 gulp.doneCallback = function (err) {
