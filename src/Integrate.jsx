e48e21dedbb918152b310538d849b01c3c89cc02
Zlatko Fedor <zfedor@gmail.com>
2016-04-09 11:10:00 +0200

    added integrate component

---
+++ src/Integrate.jsx
@@ -0,0 +1,44 @@
+import React, { PropTypes, cloneElement } from 'react';
+import Element from './Element';
+
+export default class Integrate extends Element {
+  static isElement = Element.isElement;
+
+  static propTypes = {
+    ...Element.propTypes,
+    children: PropTypes.node,
+    // todo remove originalProps and add new props as additional variable
+    /*
+    onChange: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.bool,
+    ]),
+    value: PropTypes.string,*/
+  };
+
+  static defaultProps = {
+    onChange: true,
+    value: 'value',
+  };
+
+  render() {
+    const { children, originalProps = {}, onChange, value } = this.props;
+    const { onChange: onChangeOriginal, value: originalValue } = originalProps;
+
+    const newProps = {};
+
+    if (onChangeOriginal !== false) {
+      const onChangePropName = onChangeOriginal === 'string'
+        ? onChangeOriginal
+        : 'onChange';
+
+      newProps[onChangePropName] = (newValue) => onChange(newValue, this);
+    }
+
+    if (originalValue) {
+      newProps[originalValue] = value;
+    }
+
+    return cloneElement(children, newProps);
+  }
+}
