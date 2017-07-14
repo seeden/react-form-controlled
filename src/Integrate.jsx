e7e58747ca90daa46a0694d9900f8b40e134f9e4
Zlatko Fedor <zfedor@gmail.com>
2017-07-14 14:12:15 +0200

    Release 0.18.3

--- src/Integrate.jsx
+++ src/Integrate.jsx
@@ -33,9 +33,7 @@ export default class Integrate extends Element {
         value: currentValue,
         onChange: this.handleChange,
       });
-    }
-
-    if (typeof children === 'function') {
+    } else if (typeof children === 'function') {
       return children({
         name,
         value: currentValue,
